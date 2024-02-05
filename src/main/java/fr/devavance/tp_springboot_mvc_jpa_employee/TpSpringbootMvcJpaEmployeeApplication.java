f14868ee0aea6cbc1b84a123bf6309c9953d2af7
heymen <ay.alloune@gmail.com>
2024-02-05 15:36:48 +0100

    premier commit

---
+++ src/main/java/fr/devavance/tp_springboot_mvc_jpa_employee/TpSpringbootMvcJpaEmployeeApplication.java
@@ -0,0 +1,15 @@
+package fr.devavance.tp_springboot_mvc_jpa_employee;
+
+import fr.devavance.tp_springboot_mvc_jpa_employee.dao.EmployeeDAOImpl;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpSpringbootMvcJpaEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpSpringbootMvcJpaEmployeeApplication.class, args);
+	}
+	EmployeeDAOImpl employees = new EmployeeDAOImpl();
+
+}
---
+++ src/test/java/fr/devavance/tp_springboot_mvc_jpa_employee/TpSpringbootMvcJpaEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.devavance.tp_springboot_mvc_jpa_employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpSpringbootMvcJpaEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
